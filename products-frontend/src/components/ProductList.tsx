c7e219cc31914e60714e4e69e4e8e51d8cb9b47b
soniaobasuyi <obasuyisonia@gmail.com>
2025-04-26 22:52:26 +0100

    minor refactor

--- products-frontend/src/components/ProductList.tsx
+++ products-frontend/src/components/ProductList.tsx
@@ -32,13 +32,13 @@ export default function ProductList() {
       {products && !error && !loading && (
         <div className='p-8'>
           <h1 className='text-2xl font-semibold mb-6'>Products</h1>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
             {products.map((product) => (
               <div key={product.id} className='border rounded-lg p-4 shadow-md hover:scale-105 transition-scale duration-300'>
                 <h2 className='text-lg font-semibold'>{product.name}</h2>
                 <p className='text-gray-600'>{product.description}</p>
                 <p className='mt-2 font-semibold text-green-700'>${product.price}</p>
-                <p className='mt-2 text-red-700'>{product.qty} left</p>
+                <p className='mt-2 text-red-500'>{product.qty} left</p>
               </div>
             ))}
           </div>
