042b835053c1990e1c299d05e26359dd00d4fefc
soniaobasuyi <obasuyisonia@gmail.com>
2025-04-27 16:57:07 +0100

    completed the create product modal interface

--- products-frontend/src/components/ProductList.tsx
+++ products-frontend/src/components/ProductList.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import axiosInstance from '../services/axios.ts';
 import { ProductInterface } from '../interfaces/product.interface.ts';
+import ProductModal from './ProductModal.tsx';
 
 export default function ProductList() {
   const [products, setProducts] = useState<ProductInterface[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -29,19 +31,35 @@ export default function ProductList() {
       {loading && <p>Loading...</p>}
       {error && <p>Something went wrong!</p>}
       {products && !error && !loading && (
-        <div className='p-8'>
-          <h1 className='text-2xl font-semibold mb-6'>Products</h1>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-            {products.map((product) => (
-              <div key={product.id} className='border rounded-lg p-4 shadow-md hover:scale-105 transition-scale duration-300'>
-                <h2 className='text-lg font-semibold'>{product.name}</h2>
-                <p className='text-gray-600'>{product.description}</p>
-                <p className='mt-2 font-semibold text-green-700'>${product.price}</p>
-                <p className='mt-2 text-red-700'>{product.qty} left</p>
-              </div>
-            ))}
+        <>
+          <button
+            className='mb-6 p-2 bg-blue-500 text-white rounded shadow-lg hover:bg-blue-600'
+            onClick={() => setIsModalOpen(true)}
+          >
+            Create New Product
+          </button>
+
+          <ProductModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title='Create Product' />
+
+          <div className='p-8'>
+            <h1 className='text-2xl font-semibold mb-6'>Products</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className='border rounded-lg p-4 shadow-md hover:scale-105 transition-scale duration-300'
+                >
+                  <h2 className='text-lg font-semibold'>{product.name}</h2>
+                  <p className='text-gray-600'>{product.description}</p>
+                  <p className='mt-2 font-semibold text-green-700'>
+                    ${product.price}
+                  </p>
+                  <p className='mt-2 text-red-700'>{product.qty} left</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
--- products-frontend/src/components/ProductModal.tsx
+++ products-frontend/src/components/ProductModal.tsx
@@ -1,28 +1,87 @@
 import { ModalProps } from '../interfaces/modal.interface.ts';
+import { useState } from 'react';
+import * as React from 'react';
+import axiosInstance from '../services/axios.ts';
 
-export default function Modal({ isOpen, onClose, children, title }: ModalProps) {
+export default function ProductModal({ isOpen, onClose, title }: ModalProps) {
   if (!isOpen) return null;
+
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [price, setPrice] = useState<number>(0);
+  const [qty, setQty] = useState<number>(0);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newProduct = { name, description, price, qty, };
+
+    try {
+      const response = await axiosInstance.post('/product', newProduct);
+      console.log('New product created!', response.data);
+    } catch (error) {
+      console.error('Error creating product:', error);
+    }
+  };
 
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
       <div className='bg-white rounded-lg w-full max-w-md'>
 
-        <div className='flex justify-between items-center border-b p-4'>
+        <div className='flex justify-between items-center p-4'>
           <h2 className='text-lg font-bold'>{title}</h2>
           <button onClick={onClose} className='text-gray-500 hover:text-gray-700'>&times;</button>
         </div>
 
         <div className='p-4'>
-          {children}
-        </div>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-4'>
+              <label className='block mb-1'>Name</label>
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className='w-full border rounded p-2'
+                placeholder='Enter product name'
+              />
+            </div>
 
-        <div className='flex justify-end gap-2 border-t p-4'>
-          <button
-            onClick={onClose}
-            className='p-2 text-gray-700 hover:bg-gray-100 rounded-lg'
-          >
-            Cancel
-          </button>
+            <div className='mb-4'>
+              <label className='block mb-1'>Description</label>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className='w-full border rounded p-2'
+                placeholder='Enter product description'
+              />
+            </div>
+
+            <div className='mb-4'>
+              <label className='block mb-1'>Price</label>
+              <input
+                type='number'
+                value={price}
+                onChange={(e) => setPrice(Number(e.target.value))}
+                className='w-full border rounded p-2'
+              />
+            </div>
+
+            <div className='mb-4'>
+              <label className='block mb-1'>Quantity</label>
+              <input
+                type='number'
+                value={qty}
+                onChange={(e) => setQty(Number(e.target.value))}
+                className='w-full border rounded p-2'
+              />
+            </div>
+
+            <div className='flex justify-end gap-2'>
+              <button type='button' onClick={onClose} className='p-2 bg-gray-300 hover:shadow-lg rounded-lg'>Cancel</button>
+              <button type='submit' className='px-4 py-2 bg-blue-500 text-white hover:shadow-lg rounded-lg'>Save</button>
+            </div>
+
+          </form>
         </div>
       </div>
     </div>
