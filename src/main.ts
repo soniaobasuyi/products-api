add5ebbcc7f068f3c28d8ef85d6079b9b907743d
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-04 15:17:10 +0100

    minor refactor

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'production' ? true : 'http://localhost:5173',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
